f904d972858bd89355e1033c3e196b7d798a1e27
Bryan Robinson <bryanlrobinson@gmail.com>
2018-04-06 21:31:29 -0500

    Increases ServiceWorker to store in separate caches and have an offline page

---
+++ offline.md
@@ -0,0 +1,9 @@
+---
+title: Thanks for reaching out
+permalink: "/offline/"
+layout: interior
+comments: 'off'
+baseLayout: container--center
+---
+
+Your currently not connected to the internet and haven't cached this page.
--- service-workers.js
+++ service-workers.js
@@ -1,35 +1,108 @@
 // Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
 // http://creativecommons.org/publicdomain/zero/1.0/
+'use strict';
 
-// HTML files: try the network first, then the cache.
-// Other files: try the cache first, then the network.
-// Both: cache a fresh version if possible.
-// (beware: the cache will grow and grow; there's no cleanup)
+const version = 'v0.1.1::';
+const staticCacheName = version + 'static';
+const pagesCacheName = 'pages';
+const imagesCacheName = 'images';
 
-const cacheName = 'files';
+const cacheList = [
+    staticCacheName,
+    pagesCacheName,
+    imagesCacheName
+];
 
-addEventListener('fetch',  fetchEvent => {
-  const request = fetchEvent.request;
+const offlinePages = [
+    '/',
+    '/blog/',
+    '/experiments/',
+    '/speaking/',
+    '/portfolio/',
+];
+
+function updateStaticCache() {
+    return caches.open(staticCacheName)
+        .then( cache => {
+            // These items must be cached for the Service Worker to complete installation
+            return cache.addAll([
+                '/js/prism.js',
+                '/style.css',
+                '/offline'
+            ]);
+        });
+}
+
+function stashInCache(cacheName, request, response) {
+    caches.open(cacheName)
+        .then( cache => cache.put(request, response) );
+}
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(updateStaticCache()
+      .then( () => self.skipWaiting() )
+  );
+});
+
+
+
+self.addEventListener('fetch', event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Ignore non-GET requests
   if (request.method !== 'GET') {
-    return;
+      return;
   }
-  fetchEvent.respondWith(async function() {
-    const responseFromFetch = fetch(request);
-    fetchEvent.waitUntil(async function() {
-      const responseCopy = (await responseFromFetch).clone();
-      const myCache = await caches.open(cacheName);
-      await myCache.put(request, responseCopy);
-    }());
-    if (request.headers.get('Accept').includes('text/html')) {
-      try {
-        return await responseFromFetch;
-      }
-      catch(error) {
-        return caches.match(request);
-      }
-    } else {
-      const responseFromCache = await caches.match(request);
-      return responseFromCache || responseFromFetch;
-    }
-  }());
+
+  // For HTML requests, try the network first, fall back to the cache, finally the offline page
+  if (request.headers.get('Accept').includes('text/html')) {
+
+      event.respondWith(
+          fetch(request)
+              .then( response => {
+                  // NETWORK
+                  // Stash a copy of this page in the pages cache
+                  let copy = response.clone();
+                  if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
+                      stashInCache(staticCacheName, request, copy);
+                  } else {
+                      stashInCache(pagesCacheName, request, copy);
+                  }
+                  return response;
+              })
+              .catch( () => {
+                  // CACHE or FALLBACK
+                  return caches.match(request)
+                      .then( response => response || caches.match('/offline') );
+              })
+      );
+      return;
+  }
+
+  // For non-HTML requests, look in the cache first, fall back to the network
+  event.respondWith(
+      caches.match(request)
+          .then(response => {
+              // CACHE
+              return response || fetch(request)
+                  .then( response => {
+                      // NETWORK
+                      // If the request is for an image, stash a copy of this image in the images cache
+                      if (request.headers.get('Accept').includes('image')) {
+                          let copy = response.clone();
+                          stashInCache(imagesCacheName, request, copy);
+                      }
+                      return response;
+                  })
+                  .catch( () => {
+                      // OFFLINE
+                      // If the request is for an image, show an offline placeholder
+                      if (request.headers.get('Accept').includes('image')) {
+                          return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store'}});
+                      }
+                  });
+          })
+  );
 });
