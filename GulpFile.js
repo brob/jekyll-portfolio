159519d052e29da192e5616a443e9116d127ed54
Bryan Robinson <bryanlrobinson@gmail.com>
2018-07-13 11:38:28 -0500

    Style guide Addition (#57)
    * First pass at a few styleguide components
    
    * Adds sub-component reference
    
    * Converts to nunucks'
    
    * Converts to nunucks'
    
    * updates gitignore for styleguide
    
    * Adds fractal to build step
    
    * Updates gulpfile to deal with two destinations for css
    
    * updates post-head variant name
    
    * Sets up netlify toml file for deploy settings
    
    * Sets up netlify toml file for deploy settings
    
    * Fixes netlify.toml syntax
    
    * Finishes gulp command
    
    * Adds comments for future brob

--- GulpFile.js
+++ GulpFile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    fractal = require('./styleguide/fractal.js'),
+    logger = fractal.cli.console,
     imagemin = require('gulp-imagemin'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
@@ -9,6 +11,7 @@ var gulp = require('gulp'),
     run = require('gulp-run'),
     runSequence = require('run-sequence'),
     gutil = require('gulp-util');
+    fractal.web.set('builder.dest', 'styles'); // destination for the static export
 
 
 // gulp.task('default', ['sass', 'sass:watch']);
@@ -31,12 +34,32 @@ gulp.task('sass', function() {
        .pipe(sass().on('error', sass.logError))
        .pipe(autoprefixer({grid: false}))
        .pipe(cleanCSS())
+       .pipe(gulp.dest('./styleguide/public/css/'))
        .pipe(gulp.dest('./'))
 });
 
 
 gulp.task('sass:watch', function () {
-    gulp.watch(['scss/**/*.scss', 'scss/*.scss'], ['sass']);
+    gulp.watch(['scss/**/*.scss', 'scss/*.scss', 'styleguide/components/**/*.scss','styleguide/*.scss'], ['sass']);
+});
+
+gulp.task('fractal:start', function(){
+    const server = fractal.web.server({
+        sync: true
+    });
+    server.on('error', err => logger.error(err.message));
+    return server.start().then(() => {
+        logger.success(`Fractal server is now running at ${server.url}`);
+    });
+});
+
+gulp.task('fractal:build', function(){
+    const builder = fractal.web.builder();
+    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+    builder.on('error', err => logger.error(err.message));
+    return builder.build().then(() => {
+        logger.success('Fractal build completed!');
+    });
 });
 
 gulp.task('build:jekyll', function() {
@@ -56,9 +79,21 @@ gulp.task('serve:jekyll', function() {
 });
 
 gulp.task('default', function(callback) {
-    runSequence('sass:watch', 'serve:jekyll', callback);
+    runSequence('sass:watch', 'fractal:start', 'serve:jekyll', callback);
 });
 
+// For just working on styleguide and not running jekyll server
+gulp.task('style:watch', function(callback) {
+    runSequence('fractal:start', 'sass:watch', callback);
+});
+
+// For just building styleguide. 
+// This is the build command for the style-guide branch deploy on netlify 
+gulp.task('style:build', function(callback) {
+    runSequence('sass', 'fractal:build', callback);
+});
+
+// Master branch deploy builder
 gulp.task('build', function(callback) {
     runSequence('sass', 'build:jekyll', 'img-opt');
 });
