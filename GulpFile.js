7adbfc1ee0d8d0aaa8eb054fb81b96e74913d80c
Bryan Robinson <bryanlrobinson@gmail.com>
2018-09-29 21:56:26 -0500

    Gets the rest of status build up

--- GulpFile.js
+++ GulpFile.js
@@ -10,40 +10,45 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     run = require('gulp-run'),
     runSequence = require('run-sequence'),
-    gutil = require('gulp-util');
-    fractal.web.set('builder.dest', 'styles'); // destination for the static export
+    gutil = require('gulp-util'),
+    fs = require('file-system'),
+    request = require('request');
+fractal.web.set('builder.dest', 'styles'); // destination for the static export
 
+require('dotenv').config();
+
+var buildSrc = "./";
 
 // gulp.task('default', ['sass', 'sass:watch']);
 
-gulp.task('img-opt', function() {
+gulp.task('img-opt', function () {
 
     if (gutil.env.BRANCH === 'master') {
         return gulp.src('_site/images/**/')
-        .pipe(imagemin({
+            .pipe(imagemin({
                 progressive: true,
                 svgoPlugins: [{ removeViewBox: false }]
             }))
-        .pipe(gulp.dest('_site/images'))
+            .pipe(gulp.dest('_site/images'))
     }
 });
 
 
-gulp.task('sass', function() {
-   return gulp.src('scss/**/*.scss')
-       .pipe(sass().on('error', sass.logError))
-       .pipe(autoprefixer({grid: false}))
-       .pipe(cleanCSS())
-       .pipe(gulp.dest('./styleguide/public/css/'))
-       .pipe(gulp.dest('./'))
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ grid: false }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./styleguide/public/css/'))
+        .pipe(gulp.dest('./'))
 });
 
 
 gulp.task('sass:watch', function () {
-    gulp.watch(['scss/**/*.scss', 'scss/*.scss', 'styleguide/components/**/*.scss','styleguide/*.scss'], ['sass']);
+    gulp.watch(['scss/**/*.scss', 'scss/*.scss', 'styleguide/components/**/*.scss', 'styleguide/*.scss'], ['sass']);
 });
 
-gulp.task('fractal:start', function(){
+gulp.task('fractal:start', function () {
     const server = fractal.web.server({
         sync: true
     });
@@ -53,7 +58,7 @@ gulp.task('fractal:start', function(){
     });
 });
 
-gulp.task('fractal:build', function(){
+gulp.task('fractal:build', function () {
     const builder = fractal.web.builder();
     builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
     builder.on('error', err => logger.error(err.message));
@@ -62,7 +67,7 @@ gulp.task('fractal:build', function(){
     });
 });
 
-gulp.task('build:jekyll', function() {
+gulp.task('build:jekyll', function () {
     var shellCommand = 'bundle exec jekyll build';
 
     return gulp.src('')
@@ -70,38 +75,77 @@ gulp.task('build:jekyll', function() {
         .on('error', gutil.log);
 });
 
-gulp.task('serve:jekyll', function() {
+gulp.task('serve:jekyll', function () {
     var shellCommand = "bundle exec jekyll serve";
 
     return gulp.src('')
-    .pipe(run(shellCommand))
-    .on('error', gutil.log);
+        .pipe(run(shellCommand))
+        .on('error', gutil.log);
 });
 
-gulp.task('lambda:build', function() {
+gulp.task('lambda:build', function () {
     var shellCommand = "netlify-lambda build lambda_build";
 
     return gulp.src('')
-    .pipe(run(shellCommand))
-    .on('error', gutil.log);
+        .pipe(run(shellCommand))
+        .on('error', gutil.log);
 });
 
-gulp.task('default', function(callback) {
-    runSequence('sass:watch', 'fractal:start', 'serve:jekyll', callback);
+gulp.task('status:get', function () {
+    var url = `https://api.netlify.com/api/v1/forms/${process.env.APPROVED_COMMENTS_FORM_ID}/submissions/?access_token=${process.env.API_AUTH}`;
+    console.log(url);
+    request(url, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            var body = JSON.parse(body);
+            var statuses = [];
+            // massage the data into the shape we want,
+            for (var item in body) {
+                var data = body[item].data;
+                console.log(data);
+                var status = {
+                    status: data.doing,
+                    imgUrl: data.imgUrl,
+                    date: body[item].created_at
+                };
+
+                statuses.push(status);
+
+            }
+
+            // write our data to a file where our site generator can get it.
+            fs.writeFile(buildSrc + "/_data/statuses.json", JSON.stringify(statuses, null, 2), function (err) {
+                if (err) {
+                    console.log(err);
+                    //   done();
+                } else {
+                    console.log("Comments data saved.");
+                    //   done();
+                }
+            });
+
+        } else {
+            console.log("Couldn't get comments from Netlify");
+            //   exit();
+        }
+    });
+});
+
+gulp.task('default', function (callback) {
+    runSequence('sass:watch', 'fractal:start', 'status:get', 'serve:jekyll', callback);
 });
 
 // For just working on styleguide and not running jekyll server
-gulp.task('style:watch', function(callback) {
+gulp.task('style:watch', function (callback) {
     runSequence('fractal:start', 'sass:watch', callback);
 });
 
 // For just building styleguide. 
 // This is the build command for the style-guide branch deploy on netlify 
-gulp.task('style:build', function(callback) {
+gulp.task('style:build', function (callback) {
     runSequence('sass', 'fractal:build', callback);
 });
 
 // Master branch deploy builder
-gulp.task('build', function(callback) {
-    runSequence('sass', 'build:jekyll', 'lambda:build', 'img-opt');
+gulp.task('build', function (callback) {
+    runSequence('sass', 'status:get', 'build:jekyll', 'lambda:build', 'img-opt');
 });
--- lambda_build/brob-sight.js
+++ lambda_build/brob-sight.js
@@ -1,3 +1,9 @@
+'use strict';
+
+var request = require("request");
+
+// populate environment variables locally.
+require('dotenv').config()
 
 
 
@@ -5,15 +11,41 @@
 exports.handler = async (event, context) => {
     const doingString = event.queryStringParameters.doing;
     const imgUrl = event.queryStringParameters.imgUrl;
-    const happeningObject = {
-      'doingString': doingString,
-      'imgUrl': imgUrl
-    }
 
-    console.log(happeningObject);
+    // now we have the data, let's massage it and post it to the approved form
+    var payload = {
+      'form-name' : "happening-post",
+      'received': new Date().toString(),
+      'doing': doingString,
+      'imgUrl': imgUrl
+    };
+
+    console.log(payload);
+    var approvedURL = "https://bryanlrobinson.com";
+
+    request.post({'url':approvedURL, 'formData': payload }, function(err, httpResponse, body) {
+      var msg;
+      console.log('inside post');
+      if (err) {
+        console.log('inside error');
+
+        msg = 'Post to approved comments failed:' + err;
+        console.log(msg);
+      } else {
+        console.log('inside else');
+        console.log(httpResponse.statusCode);
+        msg = 'Post to approved comments list successful.'
+        console.log(msg);
+      }
+      var msg = "Comment registered. Site deploying to include it.";
+
+      return true;
+    });
+
+
 
     return {
       statusCode: 200,
-      body: JSON.stringify({happeningObject})
+      body: JSON.stringify({payload})
     };
   };
